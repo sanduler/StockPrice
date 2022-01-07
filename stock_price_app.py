5457da40e11df0c0b6829ce4feb95087c5f3f8b2
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-06 16:04:47 -0800

    added lavel for volume price

--- stock_price_app.py
+++ stock_price_app.py
@@ -14,7 +14,10 @@ symbolTicker = 'GOOGL'
 # get data on this ticker
 dataTicker = yf.Ticker(symbolTicker)
 # get the history of the prices for the ticker above
-dfTicker = dataTicker.history(period='1d', start='2010-5-3', end='2022-1-5')
+dfTicker = dataTicker.history(period='1d', start='2010-1-1', end='2022-1-5')
 # Open High Low Close Volume Dividends Stock Splits
 st.line_chart(dfTicker.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(dfTicker.Volume)
